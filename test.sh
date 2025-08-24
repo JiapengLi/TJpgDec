65ca7916d97878f87e58c60b8b1cf01bdaa2eaa5
Jiapeng Li <mail@jiapeng.me>
2025-08-24 19:56:12 +0800

    add debug logs

--- test.sh
+++ test.sh
@@ -5,5 +5,12 @@ JPEG_DECODER=./jpeg_decode
 IMAGES=samples/*.jpg
 
 for img in $IMAGES; do
-    $JPEG_DECODER "$img"
+    $JPEG_DECODER "$img" > samples/$(basename "$img" .jpg).txt
+done
+
+JPEG_DECODER=./jpeg_decode_debug
+IMAGES=samples/*.jpg
+
+for img in $IMAGES; do
+    $JPEG_DECODER "$img" > samples/$(basename "$img" .jpg)-debug.txt
 done
